f0748744e228386858451e80702ecd74d4a9040f
Aaron Dorrance <celnardur@celnardur.com>
2023-03-31 03:08:35 -0500

    Addapted Stuff to save plays

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ const save = window.__TAURI__.dialog.save;
 let score = [0, 0];
 let field_pos = 0;
 let plays = [];
+let save_path = null;
 
 let count = 0;
 
@@ -34,6 +35,10 @@ let counter = {
                 m("label", {for: "down"}, "Down"),
                 m("input", {type: "number", id: "down", min: "1", max: "4"}),
                 m("br"),
+
+                m("label", {for: "new_field_pos"}, "New Field Position"),
+                m("input", {type: "number", id: "new_field_pos", min: "-9", max: "9"}),
+                m("br"),
                 
                 m("label", {for: "points"}, "Points This Play"),
                 m("input", {type: "number", id: "points_negitive", min: "0", max: "9999", value: "0"}),
@@ -59,16 +64,85 @@ let counter = {
                 m("input", {type: "text", id: "notes"}),
                 m("br"),
 
-                m("input", {type: "submit", value: "Add Play", onclick: function() { add_play() } }),
+                m("input", {type: "submit", value: "Add Play", onclick: add_play }),
             ]),
-            m("div", m("button", {onclick: function() { count++; }}, count + " Clicks!")),
-            m("div", m("button", {onclick: function() { save_count(count) } }, "Save"))
+            m("br"),
+
+            m("div", m("button", {onclick: save_plays }, "Save")),
+            m("div", m("button", {onclick: save_as_plays }, "Save As")),
         ];
     }
 }
 
 function add_play() {
+    let negitive_score = Number(document.getElementById("negitive_score").value);
+    let positive_score = Number(document.getElementById("positive_score").value);
+    let old_field_pos = Number(document.getElementById("field_pos").value);
+    let clock_min = Number(document.getElementById("clock_min").value);
+    let clock_sec = Number(document.getElementById("clock_sec").value);
+    let down = Number(document.getElementById("down").value);
+    let new_field_pos = Number(document.getElementById("new_field_pos").value);
+    let points_negitive = Number(document.getElementById("points_negitive").value);
+    let points_positive = Number(document.getElementById("points_positive").value);
+    let play_type = document.getElementById("play_type").value;
+    let notes = document.getElementById("notes").value;
+
+    let play = {
+        clock: clock_min*60 + clock_sec,
+        field_pos: [old_field_pos, new_field_pos],
+        points_from_play: [points_negitive, points_positive],
+        score_after: [negitive_score + points_negitive, positive_score + points_positive],
+        down: down,
+        play_type: play_type,
+        notes: notes,
+    };
+
+    score = [negitive_score + points_negitive, positive_score + points_positive]
+    field_pos = new_field_pos;
+
+    plays.push(play);
 }
+
+if (query_params.has("game_file")) {
+    let file = query_params.get("game_file");
+    invoke('open_game', { path: file})
+        .then((in_plays) => {
+            plays = in_plays;
+            let last_play = plays[plays.length - 1];
+            field_pos = last_play.field_pos[1];
+            score = last_play.score_after;
+            save_path = file;
+
+            m.mount(root, counter);
+        }) 
+        .catch((error) => alert(error));
+} else {
+    plays = [];
+    field_pos = 0;
+    score = [0, 0];
+    save_path = null;
+
+    m.mount(root, counter);
+}
+
+async function save_plays() {
+    if (save_path == null) {
+        return await save_as_plays();
+    } else {
+        invoke('save_game', {plays: plays, path: save_path})
+            .then(() => {
+                alert("Game Saved");
+            })
+            .catch((error) => alert(error));
+    }
+}
+
+async function save_as_plays() {
+    let filepath = await save();
+    save_path = filepath;
+    save_plays();
+}
+
 
 if (query_params.has("count_file")) {
     let file = query_params.get("count_file");
--- ui/start.js
+++ ui/start.js
@@ -3,10 +3,10 @@ const open = window.__TAURI__.dialog.open;
 async function on_open() {
     const selected = await open();
     if (Array.isArray(selected)) {
-        window.location.assign("/counter.html?count_file="+selected[0]);
+        window.location.assign("/counter.html?game_file="+selected[0]);
     } else if (selected === null) {
         ;
     } else {
-        window.location.assign("/counter.html?count_file="+selected);
+        window.location.assign("/counter.html?game_file="+selected);
     }
 }
